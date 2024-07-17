bab22557c6ef4e7b714cf9f3e29bd0c281085ad0
gamerfunky78 <gamerfunky78@gmail.com>
2024-07-17 12:51:55 +0300

    Improved error handling

--- index.js
+++ index.js
@@ -15,32 +15,27 @@ async function fetchuserInfo() {
     url = (`https://api.github.com/users/${USER}`)
 
     response = await fetch(url)
-
+    
     try {
         const json = await response.json();
 
-        console.log(json);
-
-        if (json.name === null && json.login === null) {
-            window.alert("Couldn't find user.")        
+        if (json.name === undefined && json.login === undefined || USER === " " || USER === "null" ) {
+            window.alert("Please input a github username!")        
         }
         else {
             document.getElementById("user-bio").textContent = `Bio : ${json.bio}`
             document.getElementById("user-followers").textContent = `No of followers : ${json.followers}`
             document.getElementById("user-name").textContent = `Name : ${json.name}`
             document.getElementById("user-web").textContent = `Website : ${json.blog}`
-            document.getElementById("user-link").textContent = `Users Github : github.com/${json.login}`
+            document.getElementById("user-link").textContent = `Users Github : ${json.html_url}`
             document.getElementById("user-repos").textContent = `Users number of Repositories : ${json.public_repos}`
             document.getElementById("user-image").src = json.avatar_url
-            document.getElementById("users-twitter").textContent =  `Users Twitter : ${json.twitter_username}`
-            
+            document.getElementById("users-twitter").textContent =  `Users Twitter : ${json.twitter_username}`    
             document.getElementById("user-joined").textContent =  `User Joined on : ${json.created_at}`
         }
-
     }
-
     catch (error) {
-        console.log("Error while fetching user.")
+        window.alert("Error while fetching user.")
     }
 
 
